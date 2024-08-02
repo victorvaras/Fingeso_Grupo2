8c06fa0eaac0134a9fe8719481c066f1e59816b9
victor <victor.varas@gmail.com>
2024-08-02 19:43:13 -0400

    Spring boot maven

---
+++ Fingeso/Fingeso/src/main/java/com/example/Fingeso/FingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingesoApplication.class, args);
+	}
+
+}
---
+++ Fingeso/Fingeso/src/test/java/com/example/Fingeso/FingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
