1fabea25ffd7d0cac67f8933843d8d8caee55ff7
Aylin Castillo <aylin.castillo@usach.cl>
2024-08-05 21:45:48 -0400

    avance con tablas (pequeño cambio)

--- Fingeso/Fingeso/src/main/java/com/example/Fingeso/Repository/PropiedadRepository.java
+++ Fingeso/Fingeso/src/main/java/com/example/Fingeso/Repository/PropiedadRepository.java
@@ -2,9 +2,9 @@ package com.example.Fingeso.Repository;
 
 import com.example.Fingeso.Entity.PropiedadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
 
-@RestController
+@Repository
 public interface PropiedadRepository extends JpaRepository<PropiedadEntity, Integer> {
 
     PropiedadEntity findByRol(int rol);
